fc2e7073b5211768a722811cc9a79a0895b5e347
neo1seok <neo1seok@gmail.com>
2018-04-06 20:26:39 +0900

    update 4 login

--- class_web_app.py
+++ class_web_app.py
@@ -1,8 +1,11 @@
+import random
+
 import pymysql
 import time
 from flask import render_template, json, session, request
 import collections
-from neolib import neoutil
+from neolib import neoutil,crypto_util_bin
+from neolib.hexstr_util import *
 from werkzeug.utils import redirect
 
 import tool_xls_to_json
@@ -145,9 +148,10 @@ class WebAppBase():
 		self.data.from_dict(self.__dict__)
 		# uid_prefix = neoutil.get_safe_mapvalue(self.__dict__,"uid_prefix","")
 		# self.data = neoutil.Struct(uid_prefix = uid_prefix)
+		self.post_data = neoutil.Struct(**data)
 		self.data.from_dict({ key:value for key,value in data.items()})
 		self.cmd = cmd
-		print("do_query", self.cmd,self.data.get_dict())
+		print("do_query", self.cmd,self.post_data.get_dict())
 		ret = "NOT ASSIGN"
 		self.map_ret = dict(result="OK")
 		try:
@@ -192,6 +196,25 @@ class BaseDBWebApp(WebAppBase):
 	def init(self):
 
 		self.list_col_name = self.column_names.split("|")
+		self.list_tables = [
+			dict(title="title",
+			     modal_id="id_modal_input",
+			     form_id="form_input",
+			     form_title="title",
+			     id_div_list ="div_list",
+				new_input_button ="새 글쓰기",
+			     button_id="",
+			     enable_function="enable_input",
+				 input_class ="neo_form",
+			     edit_function="edit_content",
+			     delete_function="delete_content",
+			     new_input_function="new_input_function",
+			     query_option="",
+
+			     list_input_row=[],
+			     list_data=[],
+			     list_col_info=[  ])
+		 ]
 		#print("list_col_name",self.list_col_name)
 
 		pass
@@ -312,16 +335,9 @@ class WebtoonWebApp(BaseDBWebApp):
 			row_type='all',type="select",
 			list_options=[ dict(value=map_portal['prt_uid'],name=map_portal["title"]) for map_portal in list_portals])
 		                  ]
+		#self.list_tables
 
-		self.list_tables =[
-			dict(title="talbe",
-			     modal_id = "id_modal_input",
-			     form_id="form_input", form_title="title",
-			     button_id="",
-			     check_box_id="id_check_box",
-			     edit_function="edit_content",
-			     new_input_function="new_input_function",
-
+		this_table =	dict(title="웹툰",
 			     list_input_row=list_input_row,
 			     list_col_info=[
 				dict(title="제목",type="link",href_key="list_url",title_key="title"),
@@ -330,7 +346,8 @@ class WebtoonWebApp(BaseDBWebApp):
 				dict(title="삭제", type="btn_no_modal",onclick="delete_content"),
 			],
 			     list_data = self.list_data)
-		]
+
+		self.list_tables[0].update(this_table)
 		neoutil.simple_view_list(self.list_data)
 		pass
 	def post_process_old(self):
@@ -359,23 +376,17 @@ class FavLinkDBWebApp(BaseDBWebApp):
 
 		]
 
-		self.list_tables = [
-			dict(title="전체리스트",
-			     modal_id="id_modal_input",
-			     form_id="form_input", form_title="title",
-			     button_id="",
-			     check_box_id="id_check_box",
-			     edit_function="edit_content",
-			     new_input_function="new_input_function",
-
-			     list_input_row=list_input_row,
-			     list_col_info=[
+		this_table = 			dict(title="전체리스트",
+			   form_title="개인링크",
+			   list_input_row=list_input_row,
+			   list_col_info=[
 				     dict(title="링크", type="link", href_key="link", title_key="title"),
 				     dict(title="편집", type="btn", onclick="edit_content"),
 				     dict(title="삭제", type="btn_no_modal", onclick="delete_content"),
 			     ],
 			     list_data=self.list_data)
-		]
+
+		self.list_tables[0].update(this_table)
 		neoutil.simple_view_list(self.list_data)
 		pass
 	# def is_login(self):
@@ -404,15 +415,8 @@ class TodayContentsWebApp(BaseDBWebApp):
 		list_attr_ext = [dict(key="onclick", proc=lambda item:"edit_content('{cur_uid}')".format(cur_uid=item['cur_uid']))]
 		text_ext = lambda item: item['title']
 
-
-		self.list_tables = [
-			dict(title="전체리스트",
-			     modal_id="id_modal_input",
-			     form_id="form_input", form_title="title",
-			     button_id="",
-			     check_box_id="id_check_box",
-			     edit_function="edit_content",
-			     new_input_function="new_input_function",
+		this_table = dict(title="전체리스트",
+				form_title="title",
 
 			     list_input_row=list_input_row,
 			     list_col_info=[
@@ -427,7 +431,8 @@ class TodayContentsWebApp(BaseDBWebApp):
 				     dict(title="삭제", type="btn_no_modal", onclick="delete_content"),
 			     ],
 			     list_data=self.list_data)
-		]
+
+		self.list_tables[0].update(this_table)
 		neoutil.simple_view_list(self.list_data)
 		pass
 
@@ -537,21 +542,37 @@ class PasswdWebApp(BaseDBWebApp):
 		pass
 	pass
 	def post_process(self):
+		self.list_header = self.select("SELECT phd_uid, title, hint, special_letter FROM pheader;")
 
 
 		list_input_site = [
 
 			dict(name="cur_uid", id="input_cur_uid", type='hidden'),
 			dict(title="site", name="site", id="input_site", row_type="left", type="input"),
-			dict(title="header", name="phd_uid", id="input_header", row_type="right", type="select"),
+			dict(title="header", name="phd_uid", id="input_header", row_type="right",
+			     type="select",
+			     list_options = [ dict(value=map_portal['phd_uid'], name=map_portal["title"]) for map_portal in self.list_header]),
 			dict(title="ptail", name="ptail", id="input_ptail", row_type="left", type="input"),
 			dict(title="id", name="ptail", id="input_id", row_type="right", type="input"),
 			dict(title="etc", name="etc", id="input_etc", row_type="all", type="text"),
 
 		]
+		list_input_header = [
+
+			dict(name="phd_uid", id="input_phd_uid", type='hidden'),
+			dict(title="헤더이름", name="title", id="input_title_header", row_type="left", type="input"),
+			dict(title="특수문자", name="special_letter", id="input_special_letter", row_type="right", type="input"),
+			dict(title="힌트", name="hint", id="input_hint", row_type="all", type="input"),
+
+
+
+		]
 
 		list_attr = [dict(key="data-toggle", val="modal"),
 		             dict(key="data-target", val="#id_modal_input"),
+		             ]
+		list_attr_header = [dict(key="data-toggle", val="modal"),
+		             dict(key="data-target", val="#id_modal_input_header"),
 		             ]
 		list_attr_ext = [
 			dict(key="onclick", proc=lambda item: "edit_content('{cur_uid}')".format(cur_uid=item['cur_uid']))]
@@ -559,24 +580,68 @@ class PasswdWebApp(BaseDBWebApp):
 			dict(key="onclick", proc=lambda item: "edit_content_header('{phd_uid}')".format(phd_uid=item['phd_uid']))]
 		text_ext = lambda item: item['site']
 		text_header_ext = lambda item: item['title']
-		self.list_tables = [
-			dict(title="사이트리스트",
-			     modal_id="id_modal_input",
-			     form_id="form_input", form_title="title",
-			     button_id="",
-			     check_box_id="id_check_box",
-			     edit_function="edit_content",
-			     new_input_function="new_input_function",
 
-			     list_input_row=list_input_site,
-			     list_col_info=[
-				     dict(title="사이트", type="btn_ext",text_ext=text_ext,list_attr=list_attr,list_attr_ext=list_attr_ext),
-				     dict(title="헤더", type="btn_ext",  text_ext=text_header_ext,list_attr=list_attr,list_attr_ext=list_attr_header_ext),
-				     dict(title="테일", type="title", title_key="ptail"),
-			     ],
-			     list_data=self.list_data)
-		]
+		this_table = dict(title="사이트리스트",
+			 form_title="title",
+			query_option="site",
+		     list_input_row=list_input_site,
+
+		     list_data=self.list_data,
+		     list_col_info=[
+			     dict(title="사이트", type="btn_ext", text_ext=text_ext, list_attr=list_attr, list_attr_ext=list_attr_ext),
+			     dict(title="헤더", type="btn_ext", text_ext=text_header_ext, list_attr=list_attr_header,
+			          list_attr_ext=list_attr_header_ext),
+			     dict(title="테일", type="title", title_key="ptail"),
+			     dict(title="삭제", type="btn_no_modal", onclick="delete_content"),
+		     ])
+		this_table_header =dict(title="헤더리스트",
+		     modal_id="id_modal_input_header",
+		    id_div_list="div_list_header",
+		     form_id="form_input_header", form_title="헤더입력",
+		     button_id="",
+		     query_option="header",
+		     new_input_button="헤더 새로 입력",
+
+		     enable_function="enable_input_header",
+		     input_class="neo_form_header",
+
+		     check_box_id="id_check_box_header",
+		     edit_function="edit_content_header",
+		     delete_function="delete_content_header",
+		     new_input_function="new_input_function_header",
+
+		     list_input_row=list_input_header,
+		     list_data=self.list_header,
+		     list_col_info=[
+			     # dict(title="사이트", type="btn_ext",text_ext=text_ext,list_attr=list_attr,list_attr_ext=list_attr_ext),
+			     dict(title="헤더", type="btn_ext", text_ext=text_header_ext, list_attr=list_attr_header,
+			          list_attr_ext=list_attr_header_ext),
+
+
+		     ])
+		self.list_tables[0].update(this_table)
+		if len(self.list_tables) ==1:
+			self.list_tables.append(this_table_header)
+		else:
+			self.list_tables[1].update(this_table_header)
 		neoutil.simple_view_list(self.list_data)
+		pass
+
+	def get_content(self):
+		print('get_content',self.data.option)
+		if self.data.option == 'site':
+			return BaseDBWebApp.get_content(self)
+		elif self.data.option == 'header':
+			list_header = self.select("""SELECT phd_uid, title, hint, special_letter 
+			FROM pheader where phd_uid = '{cur_uid}';""".format(cur_uid=self.data.cur_uid ))
+
+			response = list_header[0]
+			response['result'] = "OK"
+			return response
+			pass
+		else:
+			raise Exception("not allowd option")
+
 		pass
 class TestWebApp(BaseDBWebApp):
 	def ready_extra_condition(self):
@@ -646,24 +711,57 @@ class LogOut(WebAppBase):
 
 		print("LogOut main_process",session)
 		return redirect("/")
-class LogIn(WebAppBase):
+class LogIn(BaseDBWebApp):
 	def init(self):
 		WebAppBase.init(self)
 
 
+		self.map_hint = {
+			"second_sister": "0331",
+			"first_sister": "1219",
+			"dad": "0124",
+			"mom": "1011",
+			"me": "0815",
+			"sewol_sadday": "0416",
+		}
+
+
+	def update_from_db(self):
+		pass
 	def log_in_facebook(self):
-		print("log_in_facebook data", self.data)
+		print("log_in_facebook data", self.data.fb_userid,self.data.hash_hint_passwd)
+		list_user_info = self.select("""SELECT seq, usr_uid, id, fb_userid, name, pwd, rn, rn_srv, etc_info 
+					FROM neo_pwinfo.user where fb_userid='{fb_userid}';""".format(fb_userid=self.data.fb_userid))
+		redirect = neoutil.get_safe_mapvalue(session, tag_redirect, "/")
+		print(tag_redirect, redirect)
+		if len(list_user_info) ==0:
+			raise Exception("user is not in db")
+		value = self.map_hint[self.pre_key]
+
+		calc_hash = crypto_util_bin.sha256(self.server_random+ self.map_hint[self.pre_key].encode())
+		print('calc_hash',tohexstr(calc_hash))
+		print('hash_hint_passwd', self.data.hash_hint_passwd)
+		if calc_hash != tobytes(self.data.hash_hint_passwd):
+			raise Exception("password hit is not match")
+
+
+		user_info = neoutil.Struct(**list_user_info[0])
 
 		session[tag_login] = True
-		session[tag_user] = "신원석"
+		session[tag_user] = user_info.name
 		session[tag_time] = time.time()
 		#redirect = session[tag_redirect]
-		redirect = neoutil.get_safe_mapvalue(session,tag_redirect,"/")
-		print(tag_redirect,redirect)
+
 
 		session[tag_redirect] = "/"
 		return dict(result="ok",redirect=redirect)
 
+	def do_run(self):
+		idx_a = random.randint(0,len(self.map_hint)-1)
+		self.pre_key = list(self.map_hint.keys())[idx_a]
+		self.server_random = crypto_util_bin.getrandom(32)
+		self.hexstr_server_random = tohexstr(self.server_random)
+		pass
 	# def main_process(self):
 	# 	session[tag_login] = False
 	# 	print("LogOut main_process",session)
--- static/comm.js
+++ static/comm.js
@@ -1,21 +1,23 @@
-function show_input_enable(is_enable){
+function show_input_enable(input_class,is_enable){
     console.log('show_input_enable',is_enable);
+    var obj = $('.'+input_class )
     if(is_enable){
-        $('.neo_form').removeAttr('disabled');
+        obj.removeAttr('disabled');
     }
     else{
-        $('.neo_form').attr('disabled', '');
+        obj.attr('disabled', '');
     }
+    return is_enable;
 
 }
-function temp_query(url,data,ok_process,err_process=function(err) {console.log(error);}){
+function temp_query(url,data,ok_process,err_process=function(error) {console.log(error);}){
     $.ajax({
         url: url,
         data: data,
         type: 'POST',
         dataType: "json",
         success: function(response) {
-            if(response.result == "OK"){
+            if(response.result == "ok"){
                 ok_process(response);
 
             }
@@ -26,7 +28,7 @@ function temp_query(url,data,ok_process,err_process=function(err) {console.log(e
     });
 
 }
-function confirm(url){
+function confirm_form(url){
     console.log("btn_confirm",$('#form_input').serializeArray());
 
 
@@ -52,4 +54,45 @@ function confirm(url){
 //            console.log(error);
 //        }
 //    });
+}
+
+function sha256_from_hexstr(hex_str){
+  var str = hex2a(hex_str);
+  console.log(str);
+
+  //
+  return Sha256.hash(hex_str,{ msgFormat: 'hex-bytes', outFormat: 'hex' });
+	//return crypto_util.sha256(neoutil.Text2HexString(passwd) + rn)
+}
+
+
+function toHex(str) {
+  var result = '';
+  for (var i = 0; i < str.length; i++) {
+    result += str.charCodeAt(i).toString(16);
+  }
+
+  return result.toUpperCase();
+}
+
+function hex2a(hexx) {
+  var hex = hexx.toString(); //force conversion
+  var str = '';
+  for (var i = 0; i < hex.length; i += 2)
+    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  return str;
+}
+
+function reset_message(){
+console.log('reset_message');
+$('#id_alert').hide();
+$('#id_warning').hide();
+$('#id_alert > strong').text('');
+$('#id_warning > strong').text('');
+
+}
+
+function show_message(id,msg){
+$('#'+id ).show();
+$('#'+id +" > strong").text(msg);
 }
