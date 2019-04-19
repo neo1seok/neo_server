533d37b68c5f87fcf849aac75df9836b337a1481
neo1seok <neo1seok@gmail.com>
2019-04-19 17:28:41 +0900

    ver *1.0.5
    location.href -> window.location.href

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 #[ver].[majer].[miner]
 #ver: 전체 프레임의 격변이 있을때
 #majer:큰 기능 추가가 되었을때
 #miner:버그 수정및 작은 기능 추가.
 
 __history__ = """
+* 1.0.5
+location.href -> window.location.href 
+
+	
 * 1.0.4
 location.href -> window.location.href = '/health.neo';
 
