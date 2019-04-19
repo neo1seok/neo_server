201ec6ddc25809975cdf792f6d00aa24915f99c8
neo1seok <neo1seok@gmail.com>
2019-04-19 17:24:09 +0900

    ver *1.0.2
    location.href -> location

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 #[ver].[majer].[miner]
 #ver: 전체 프레임의 격변이 있을때
 #majer:큰 기능 추가가 되었을때
 #miner:버그 수정및 작은 기능 추가.
 
 __history__ = """
+* 1.0.2
+location.href -> location
+
+	
 * 1.0.1
 update redirect bp wt system
 
--- tool/update_history_and_git.py
+++ tool/update_history_and_git.py
@@ -12,7 +12,7 @@ class CurUpdateInitAndCommit(UpdateInitAndCommit):
 if __name__ == '__main__':
 	print(neolib)
 	msg = """
-update redirect bp wt system
+location.href -> location
 
 	"""
 	CurUpdateInitAndCommit().run(msg)
