bca1954949357c738f1d04c2fa663bc79f428f36
neo1seok <neo1seok@gmail.com>
2019-12-06 16:12:33 +0900

    ver *1.1.2
    web runtime update

--- neo_server/__init__.py
+++ neo_server/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*- 	
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 """
 #[ver].[majer].[miner]
 #ver: 전체 프레임의 격변이 있을때
@@ -7,6 +7,10 @@ __version__ = "1.1.1"
 #miner:버그 수정및 작은 기능 추가.
 """
 __history__ = """
+* 1.1.2
+web runtime update
+
+	
 * 1.1.1
 web runtime update
 
