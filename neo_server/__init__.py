1ed4fabe1d056099b8b462a583ee8e63c7eaeddf
neo1seok prv dev <neo1seok@gmail.com>
2019-12-27 12:12:58 +0900

    ver *1.1.3
    1.1.3

--- neo_server/__init__.py
+++ neo_server/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*- 	
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 """
 #[ver].[majer].[miner]
 #ver: 전체 프레임의 격변이 있을때
@@ -7,6 +7,10 @@ __version__ = "1.1.2"
 #miner:버그 수정및 작은 기능 추가.
 """
 __history__ = """
+* 1.1.3
+1.1.3
+
+	
 * 1.1.2
 web runtime update
 
